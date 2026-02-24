ace49705c1ad6ea89a943c0c156791075af67114
naimur1046 <naimurrahman1046@gmail.com>
2026-02-24 14:20:26 +0600

    update  each section scrolling smooth

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
-import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Research from './pages/Research'
 import OrganizingExperience from './pages/OrganizingExperience'
+import ProfessionalExperience from './pages/ProfessionalExperience'
 
 function App() {
-
   return (
-    <div >
-  <Header />
-  <main className='flex flex-col' >
-    <section id="home" className="flex items-center justify-center  border-gray-300">
-      <Home />
-    </section>
-    <section id="about" className="flex items-center justify-center " >
-      <About />
-    </section>
-    <section id="projects" >
-      <Projects />
-    </section>
-    <section id="research">
-      <Research />
-    </section>
-    <section id="experience">
-      <OrganizingExperience/>
-    </section>
-    <section id="contact">
-      <Contact />
-    </section>
-    
-  </main>
-</div>
-
+    <div className="bg-[#0a0f1a]">
+      <Header />
+      <main className="flex flex-col">
+        <section id="home" className="pt-20">
+          <Home />
+        </section>
+        <section id="about" className="scroll-mt-20">
+          <About />
+        </section>
+        <section id="work" className="scroll-mt-20">
+          <ProfessionalExperience />
+        </section>
+        <section id="projects" className="scroll-mt-20">
+          <Projects />
+        </section>
+        <section id="research" className="scroll-mt-20">
+          <Research />
+        </section>
+        <section id="experience" className="scroll-mt-20">
+          <OrganizingExperience />
+        </section>
+        <section id="contact" className="scroll-mt-20">
+          <Contact />
+        </section>
+      </main>
+      <Footer />
+    </div>
   )
 }
 

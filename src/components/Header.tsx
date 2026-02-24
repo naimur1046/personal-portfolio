02ba4445b0567ae9ecba95d6d10d0297c81b4cee
naimur1046 <naimurrahman1046@gmail.com>
2026-02-24 13:35:31 +0600

    Add navigation of the research and publication in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container mx-auto pl-5 pr-20 py-4 flex justify-between items-center">
         <h1 className="text-4xl font-bold text-white border-3 rounded-2xl px-4 py-2">Naimur Rahman</h1>
         <nav className="hidden md:flex space-x-6">
-          {["Home", "About", "Projects", "Experience", "Contact"].map((item) => (
+          {["Home", "About", "Projects", "Research", "Experience", "Contact"].map((item) => (
             <a key={item} href={`#${item.toLowerCase()}`} className="text-2xl font-semibold text-white hover:text-[#F0F0F0]  shadow-white transition">
               {item}
             </a>
@@ -26,7 +26,7 @@ const Header = () => {
       {isOpen && (
         <div className="md:hidden bg-white shadow-md">
           <nav className="flex flex-col items-center space-y-4 py-4">
-            {["Home", "About", "Work", "Blog", "Contact", "My Interest"].map((item) => (
+            {["Home", "About", "Projects", "Research", "Experience", "Contact"].map((item) => (
               <a key={item} href={`#${item.toLowerCase()}`} className="text-gray-600 hover:text-blue-500 transition">
                 {item}
               </a>
